0c6c2f572fa9af0d62a9c6a0ecf1406073e95571
Ivan Ruquet <ivanruquet@gmail.com>
2025-09-09 22:40:30 -0300

    Modificacion de HibernateConfig para usar HSQLDB con variables de entorno

--- src/main/java/com/tallerwebi/config/HibernateConfig.java
+++ src/main/java/com/tallerwebi/config/HibernateConfig.java
@@ -17,26 +17,30 @@ public class HibernateConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        
+
         String dbHost = System.getenv("DB_HOST");
         String dbPort = System.getenv("DB_PORT");
         String dbName = System.getenv("DB_NAME");
         String dbUser = System.getenv("DB_USER");
         String dbPassword = System.getenv("DB_PASSWORD");
-        
+
         if (dbHost == null) dbHost = "localhost";
         if (dbPort == null) dbPort = "3306";
         if (dbName == null) dbName = "tallerwebi";
         if (dbUser == null) dbUser = "user";
         if (dbPassword == null) dbPassword = "user";
-        
-        String url = String.format("jdbc:mysql://%s:%s/%s?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true", 
-                                 dbHost, dbPort, dbName);
-        
-        dataSource.setUrl(url);
+
+//        String url = String.format("jdbc:mysql://%s:%s/%s?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true",
+//                                 dbHost, dbPort, dbName);
+
+//        dataSource.setUrl(url);
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        dataSource.setUrl("jdbc:hsqldb:mem:db_");
         dataSource.setUsername(dbUser);
         dataSource.setPassword(dbPassword);
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
         return dataSource;
     }
 
@@ -56,7 +60,8 @@ public class HibernateConfig {
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
         properties.setProperty("hibernate.hbm2ddl.auto", "create");
